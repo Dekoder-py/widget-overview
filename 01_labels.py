73c3712f511e7329bfdde8795f26793bdc4073d7
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-26 06:59:09 +1300

    add an image and a greeting with stringVar()

--- 01_labels.py
+++ 01_labels.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 from windows import set_dpi_awareness
 
 set_dpi_awareness()
@@ -9,9 +10,15 @@ root.geometry("600x400")
 root.resizable(False, False)
 root.title("Widget Examples")
 
-label = ttk.Label(root, text="Hello, world!", padding=20)
-label.config(font=("SF Pro", 40))
-label.pack()
+greeting = tk.StringVar()
 
+image = Image.open("test_image.png").resize((64, 64))
+photo = ImageTk.PhotoImage(image)
+label = ttk.Label(root, text="Purple square", image=photo, padding=5, compound="right")
+label.pack()
+greeting_label = ttk.Label(root, textvariable=greeting, padding=10)
+greeting_label.pack()
+
+greeting.set("Hello there!")
 
 root.mainloop()
